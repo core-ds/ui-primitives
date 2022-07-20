379d536a40c37cf6bf6d98c225edaa9394e363f1
Alex Yatsenko <reme3d2y@gmail.com>
2022-07-20 21:06:50 +0700

    fix: dirty release, sorry

--- utils/ci.sh
+++ utils/ci.sh
@@ -4,31 +4,9 @@
 git fetch --prune --unshallow
 git fetch --tags
 
-# Смотрим, были ли изменения в папках icons и styles
-LATEST_TAG=$(git describe --tags --abbrev=0)
-CHANGES=$(git diff --name-only $LATEST_TAG HEAD | grep -E "(icons|styles)/")
-MAJOR_CHANGES=$(git diff --name-only $LATEST_TAG HEAD --diff-filter=D | grep -E "(icons|styles)/")
+npm ci
 
-# Если были изменения, релизим новую версию пакета
-if [ -z "$CHANGES" ]
-then
-    echo "No changes found"
-else
-    echo $CHANGES
-    echo "Changes found, publish..."
+git config --global user.email "ds@gitmax.tech"
+git config --global user.name "core-ds-bot"
 
-    npm ci
-
-    git config --global user.email "ds@gitmax.tech"
-    git config --global user.name "core-ds-bot"
-
-    # Если были мажорные изменения, релизим мажорную версию пакета
-    if [ -z "$MAJOR_CHANGES" ]
-    then
-        echo "release-minor"
-        npm run release-minor
-    else
-        echo "release-major"
-        npm run release-major
-    fi
-fi
+npm run release-minor
